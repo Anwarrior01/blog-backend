d1aaf1692d7c5c59da5e46757640252bfec888ab
Anwar Elaajabi <anwar.elaajabi1999@gmail.com>
2026-05-04 13:13:14 +0100

    migrations set-up

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,3 +1,4 @@
+import { DataSource } from "typeorm";
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions.js";
 
 const config : PostgresConnectionOptions = {
@@ -8,7 +9,9 @@ const config : PostgresConnectionOptions = {
     password : "1234",
     database : "blog",
     entities : [__dirname + '/**/*.entity.{ts,js}'],
-    synchronize : true,
+    migrationsTableName : 'migrations',
+    migrations : [__dirname + '/migrations/**/*.ts'],
 }
-
+const AppDataSource = new DataSource(config);
+export {AppDataSource}
 export default config;
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,16 @@
 import { CreateUserDTO } from '@/user/dto/createUser.dto';
+import { UserEntity } from '@/user/user.entity';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-   createUser(createUserDTO:CreateUserDTO):CreateUserDTO{
-    return createUserDTO
-}
+    constructor(@InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>){}
+   async createUser(createUserDTO:CreateUserDTO):Promise<CreateUserDTO>{
+    const newUser = new UserEntity();
+    Object.assign(newUser, createUserDTO);
+    await this.userRepository.save(newUser);
+    return createUserDTO;
+    }
 }
