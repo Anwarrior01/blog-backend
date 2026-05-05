c85ba9c67e8f640752b4d10e1663050cc171b539
Anwar Elaajabi <anwar.elaajabi1999@gmail.com>
2026-05-05 05:19:38 +0100

    auth middleware implementation done

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,22 +1,28 @@
+import type { AuthRequest } from '@/types/expressRequest.interface';
 import { CreateUserDTO } from '@/user/dto/createUser.dto';
 import { LoginUserDTO } from '@/user/dto/loginUser.dto';
 import { IUserResponse } from '@/user/types/userResponse.interface';
 import { UserService } from '@/user/user.service';
-import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UsePipes, ValidationPipe } from '@nestjs/common';
 
-@Controller('users')
+@Controller()
 export class UserController {
-    constructor ( private readonly userService:UserService){}
+    constructor(private readonly userService: UserService) { }
 
-    @Post()
+    @Post('users')
     @UsePipes(new ValidationPipe)
-async createUser(@Body('user') createUserDTO : CreateUserDTO) : Promise<IUserResponse>{
-    return await  this.userService.createUser(createUserDTO);
-}
-@Post('login')
-@UsePipes(new ValidationPipe)
-async loginUser(@Body('user') loginUserDTO : LoginUserDTO) : Promise<IUserResponse>{
-    const user = await this.userService.loginUser(loginUserDTO);
-    return this.userService.generateUserResponse(user);
-}
+    async createUser(@Body('user') createUserDTO: CreateUserDTO): Promise<IUserResponse> {
+        return await this.userService.createUser(createUserDTO);
+    }
+    @Post('users/login')
+    @UsePipes(new ValidationPipe)
+    async loginUser(@Body('user') loginUserDTO: LoginUserDTO): Promise<IUserResponse> {
+        const user = await this.userService.loginUser(loginUserDTO);
+        return this.userService.generateUserResponse(user);
+    }
+    @Get('user')
+    async getCurrentUser(@Req() request:AuthRequest): Promise<IUserResponse> {
+        // console.log("request : " , request)
+        return this.userService.generateUserResponse(request.user);
+    } 
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,67 +1,112 @@
 import { CreateUserDTO } from '@/user/dto/createUser.dto';
+import { LoginUserDTO } from '@/user/dto/loginUser.dto';
 import { IUserResponse } from '@/user/types/userResponse.interface';
 import { UserEntity } from '@/user/user.entity';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { sign } from 'jsonwebtoken';
+import { compare } from 'bcrypt';
 import { Repository } from 'typeorm';
-import { sign } from "jsonwebtoken"
-import {compare} from "bcrypt"
-import { LoginUserDTO } from '@/user/dto/loginUser.dto';
+
 @Injectable()
 export class UserService {
-    constructor(@InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>) { }
-    async createUser(createUserDTO: CreateUserDTO): Promise<IUserResponse> {
-        const newUser = new UserEntity();
-        Object.assign(newUser, createUserDTO);
-        const userByEmail = await this.userRepository.findOne({
-            where: {
-                email: createUserDTO.email
-            }
-        })
-        const userByUsername = await this.userRepository.findOne({
-            where: {
-                username: createUserDTO.username
-            }
-        })
-        if (userByEmail || userByUsername) {
-            throw new HttpException('User with this email or username already exists', HttpStatus.UNPROCESSABLE_ENTITY);
-        }
-        const savedUser = await this.userRepository.save(newUser);
-        return this.generateUserResponse(savedUser);
-    }
-    async loginUser(loginUserDTO: LoginUserDTO): Promise<UserEntity> {
-        const user = await this.userRepository.findOne({
-            where: {
-                email: loginUserDTO.email
-            }
-        })
-        if (!user) {
-            throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
-        }
-        const matchedPassword = await compare(loginUserDTO.password, user.password);
-         if (!matchedPassword) {
-            throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
-        }
-        delete user.password;
-        return user;
-    }
-    generateToken(user: UserEntity): string {
-        return sign(
-            {
-                id: user.id,
-                username: user.username,
-                email: user.email
-            },
-            process.env.JWT_SECRET,
-        )
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+  async createUser(createUserDto: CreateUserDTO): Promise<IUserResponse> {
+    const newUser = new UserEntity();
+    Object.assign(newUser, createUserDto);
 
+    const userByEmail = await this.userRepository.findOne({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+
+    const userByUsername = await this.userRepository.findOne({
+      where: {
+        username: createUserDto.username,
+      },
+    });
+
+    if (userByEmail || userByUsername) {
+      throw new HttpException(
+        'Email or username is already taken',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
     }
-    generateUserResponse(user: UserEntity): IUserResponse {
-        return {
-            user: {
-                ...user,
-                token: this.generateToken(user)
-            }
-        }
+
+    const savedUser = await this.userRepository.save(newUser);
+    return this.generateUserResponse(savedUser);
+  }
+
+  async loginUser(loginUserDto: LoginUserDTO): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({
+      where: {
+        email: loginUserDto.email,
+      },
+    });
+
+    if (!user) {
+      throw new HttpException(
+        'Wrong email or password',
+        HttpStatus.UNAUTHORIZED,
+      );
     }
+
+    const matchPassword = await compare(loginUserDto.password, user.password!.toString() );
+
+    if (!matchPassword) {
+      throw new HttpException(
+        'Wrong email or password',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    delete user.password;
+
+    return user;
+  }
+
+  async findById(id: number): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      throw new HttpException(
+        `User with ID ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    return user;
+  }
+
+  generateToken(user: UserEntity): string {
+    return sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+      process.env.JWT_SECRET as string,
+    );
+  }
+
+  generateUserResponse(user: UserEntity): IUserResponse {
+    if (!user.id) {
+      throw new HttpException('User data is missing', HttpStatus.BAD_REQUEST);
+    }
+
+    return {
+      user: {
+        ...user,
+        token: this.generateToken(user),
+      },
+    };
+  }
 }
